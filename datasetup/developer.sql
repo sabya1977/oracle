9c29bb880c069b9eea6244eb05fdc911ef72efea
sabyasachi <sabyasachihyd2003@gmail.com>
2025-02-12 01:01:33 +0530

    redeploy dev
    redeploy dev

---
+++ datasetup/developer.sql
@@ -0,0 +1,70 @@
+create role DEVELOPER NOT IDENTIFIED;
+--
+grant READ ANY TABLE to developer;
+--
+grant CREATE SESSION to developer;
+--
+grant SELECT_CATALOG_ROLE to developer;
+--
+grant resource to developer;
+--
+grant CREATE JOB to developer;
+grant GRANT ANY OBJECT PRIVILEGE to developer;
+grant DROP ANY MATERIALIZED VIEW to developer;
+grant CREATE MATERIALIZED VIEW to developer;
+grant DROP ANY PROCEDURE to developer;
+grant ALTER ANY TABLE to developer;
+grant ON COMMIT REFRESH to developer;
+grant ALTER ANY TYPE to developer;
+grant ALTER ANY MATERIALIZED VIEW to developer;
+grant EXECUTE ANY PROCEDURE to developer;
+grant ALTER SESSION to developer;
+grant ANALYZE ANY to developer;
+grant ALTER ANY PROCEDURE to developer;
+grant CREATE ANY SEQUENCE to developer;
+grant CREATE VIEW to developer;
+grant QUERY REWRITE to developer;
+grant ALTER ANY TRIGGER to developer;
+grant ALTER ANY SEQUENCE to developer;
+grant DROP ANY INDEX to developer;
+grant ALTER ANY INDEX to developer;
+grant READ ANY TABLE to developer;
+grant DROP ANY TRIGGER to developer;
+grant DROP ANY SEQUENCE to developer;
+grant DELETE ANY TABLE to developer;
+grant UPDATE ANY TABLE to developer;
+grant INSERT ANY TABLE to developer;
+grant DROP ANY TABLE to developer;
+grant CREATE ANY TABLE to developer;
+grant DROP ANY TYPE to developer;
+grant CREATE ANY PROCEDURE to developer;
+grant SELECT ANY SEQUENCE to developer;
+grant CREATE ANY TYPE to developer;
+grant CREATE ANY TRIGGER to developer;
+grant CREATE ANY INDEX to developer;
+grant SELECT ANY TABLE to developer;
+grant CREATE SESSION to developer;
+grant CREATE ANY CONTEXT to developer;
+grant CREATE ANY MATERIALIZED VIEW to developer;
+grant DROP ANY VIEW to developer;
+grant CREATE ANY VIEW to developer;
+--
+create user oradev23 identified by oracle
+default tablespace users
+temporary tablespace temp
+profile default
+account unlock;
+--
+alter user oradev23 quota unlimited on users;
+--
+grant developer to oradev23;
+--
+create user oradba23 identified by oracle
+default tablespace users
+temporary tablespace temp
+profile default
+account unlock;
+--
+alter user oradba23 quota unlimited on users;
+--
+grant dba to oradba23;
