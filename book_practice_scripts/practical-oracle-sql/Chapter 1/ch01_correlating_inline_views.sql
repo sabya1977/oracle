cf6cefd2b4c4ee5713d12195bd8a40f22f0b6c63
sabyasachi <sabyasachihyd2003@gmail.com>
2025-09-04 02:17:42 +0530

    commit new oracle folder

--- book_practice_scripts/practical-oracle-sql/Chapter 1/ch01_correlating_inline_views.sql
+++ book_practice_scripts/practical-oracle-sql/Chapter 1/ch01_correlating_inline_views.sql
@@ -16,236 +16,278 @@
    sqlcl formatting setup
    ----------------------------------------------------- */
 
-set pagesize 80
+   set pagesize 80
 set linesize 80
-set sqlformat ansiconsole
+   set sqlformat ansiconsole
 alter session set current_schema = practical;
 
 /* -----------------------------------------------------
    Chapter 1 example code
    ----------------------------------------------------- */
 
--- Listing 1-1. The yearly sales of the 3 beers from Balthazar Brauerei
+rem Listing 1-1. The yearly sales of the 3 beers from Balthazar Brauerei
+rem
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       ys.yr,
+       ys.yr_qty
+  from brewery_products bp
+  join yearly_sales ys
+on ys.product_id = bp.product_id
+ where bp.brewery_id = 518
+ order by bp.product_id,
+          ys.yr;
 
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , ys.yr
- , ys.yr_qty
-from brewery_products bp
-join yearly_sales ys
+rem Listing 1-2. Retrieving two columns from the best-selling year per beer
+rem show for each of the three beers of Balthazar Brauerei which year
+rem the most bottles of that particular beer are sold and how many bottles that were.
+rem Using two Scalar subqueries in SELECT - the query is hitting the yearly_sales twice.
+rem
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       (
+          select ys.yr
+            from yearly_sales ys
+           where ys.product_id = bp.product_id
+           order by ys.yr_qty desc
+           fetch first row only
+       ) as yr,
+       (
+          select ys.yr_qty
+            from yearly_sales ys
+           where ys.product_id = bp.product_id
+           order by ys.yr_qty desc
+           fetch first row only
+       ) as yr_qty
+  from brewery_products bp
+ where bp.brewery_id = 518
+ order by bp.product_id;
+
+rem Listing 1-3. Using just a single scalar subquery and value concatenation
+rem the code looks clumsy!
+rem
+select brewery_name,
+       product_id as p_id,
+       product_name,
+       to_number(substr(
+          yr_qty_str,
+          1,
+          instr(
+             yr_qty_str,
+             ';'
+          ) - 1
+       )) as yr,
+       to_number(substr(
+          yr_qty_str,
+          instr(
+             yr_qty_str,
+             ';'
+          ) + 1
+       )) as yr_qty,
+       yr_qty_str
+  from (
+   select bp.brewery_name,
+          bp.product_id,
+          bp.product_name,
+          (
+             select ys.yr
+                    || ';'
+                    || ys.yr_qty
+               from yearly_sales ys
+              where ys.product_id = bp.product_id
+              order by ys.yr_qty desc
+              fetch first row only
+          ) as yr_qty_str
+     from brewery_products bp
+    where bp.brewery_id = 518
+)
+ order by product_id;
+
+rem Listing 1-4. Using analytic function to be able to retrieve all columns if desired
+rem using analytic function
+
+select brewery_name,
+       product_id as p_id,
+       product_name,
+       yr,
+       yr_qty
+  from (
+   select bp.brewery_name,
+          bp.product_id,
+          bp.product_name,
+          ys.yr,
+          ys.yr_qty,
+          row_number()
+          over(partition by bp.product_id
+               order by ys.yr_qty desc
+          ) as rn
+     from brewery_products bp
+     join yearly_sales ys
    on ys.product_id = bp.product_id
-where bp.brewery_id = 518
-order by bp.product_id, ys.yr;
-
--- Listing 1-2. Retrieving two columns from the best-selling year per beer
-
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , (
-      select ys.yr
-      from yearly_sales ys
-      where ys.product_id = bp.product_id
-      order by ys.yr_qty desc
-      fetch first row only
-   ) as yr
- , (
-      select ys.yr_qty
-      from yearly_sales ys
-      where ys.product_id = bp.product_id
-      order by ys.yr_qty desc
-      fetch first row only
-   ) as yr_qty
-from brewery_products bp
-where bp.brewery_id = 518
-order by bp.product_id;
-
--- Listing 1-3. Using just a single scalar subquery and value concatenation
-
-select
-   brewery_name
- , product_id as p_id
- , product_name
- , to_number(
-      substr(yr_qty_str, 1, instr(yr_qty_str, ';') - 1)
-   ) as yr
- , to_number(
-      substr(yr_qty_str, instr(yr_qty_str, ';') + 1)
-   ) as yr_qty
-from (
-   select
-      bp.brewery_name
-    , bp.product_id
-    , bp.product_name
-    , (
-         select ys.yr || ';' || ys.yr_qty
-         from yearly_sales ys
-         where ys.product_id = bp.product_id
-         order by ys.yr_qty desc
-         fetch first row only
-      ) as yr_qty_str
-   from brewery_products bp
-   where bp.brewery_id = 518
+    where bp.brewery_id = 518
 )
-order by product_id;
-
--- Listing 1-4. Using analytic function to be able to retrieve all columns if desired
-
-select
-   brewery_name
- , product_id as p_id
- , product_name
- , yr
- , yr_qty
-from (
-   select
-      bp.brewery_name
-    , bp.product_id
-    , bp.product_name
-    , ys.yr
-    , ys.yr_qty
-    , row_number() over (
-         partition by bp.product_id
-         order by ys.yr_qty desc
-      ) as rn
-   from brewery_products bp
-   join yearly_sales ys
-      on ys.product_id = bp.product_id
-   where bp.brewery_id = 518
-)
-where rn = 1
-order by product_id;
+ where rn = 1
+ order by product_id;
 
 -- Listing 1-5. Achieving the same with a lateral inline view
+rem using lateral inline view
 
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , top_ys.yr
- , top_ys.yr_qty
-from brewery_products bp
-cross join lateral(
-   select
-      ys.yr
-    , ys.yr_qty
-   from yearly_sales ys
-   where ys.product_id = bp.product_id
-   order by ys.yr_qty desc
-   fetch first row only
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       top_ys.yr,
+       top_ys.yr_qty
+  from brewery_products bp
+--   ,   lateral ( 
+-- cross join lateral (  -- this also works - 1
+  join lateral (  -- this also works - 2
+   select ys.yr,
+          ys.yr_qty
+     from yearly_sales ys
+    where ys.product_id = bp.product_id
+    order by ys.yr_qty desc
+    fetch first row only
 ) top_ys
-where bp.brewery_id = 518
-order by bp.product_id;
+on 1 = 1 -- this also works 2
+ where bp.brewery_id = 518
+ order by bp.product_id;
 
 -- Traditional style from clause without ANSI style cross join
 
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , top_ys.yr
- , top_ys.yr_qty
-from brewery_products bp
-, lateral(
-   select
-      ys.yr
-    , ys.yr_qty
-   from yearly_sales ys
-   where ys.product_id = bp.product_id
-   order by ys.yr_qty desc
-   fetch first row only
-) top_ys
-where bp.brewery_id = 518
-order by bp.product_id;
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       top_ys.yr,
+       top_ys.yr_qty
+  from brewery_products bp,
+       lateral (
+          select ys.yr,
+                 ys.yr_qty
+            from yearly_sales ys
+           where ys.product_id = bp.product_id
+           order by ys.yr_qty desc
+           fetch first row only
+       ) top_ys
+ where bp.brewery_id = 518
+ order by bp.product_id;
 
 -- Combining both lateral and join predicates in the on clause
 
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , top_ys.yr
- , top_ys.yr_qty
-from brewery_products bp
-join lateral(
-   select
-      ys.yr
-    , ys.yr_qty
-   from yearly_sales ys
-   where ys.product_id = bp.product_id
-   order by ys.yr_qty desc
-   fetch first row only
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       top_ys.yr,
+       top_ys.yr_qty
+  from brewery_products bp
+  join lateral (
+   select ys.yr,
+          ys.yr_qty
+     from yearly_sales ys
+    where ys.product_id = bp.product_id
+    order by ys.yr_qty desc
+    fetch first row only
 ) top_ys
-   on 1=1
-where bp.brewery_id = 518
-order by bp.product_id;
+on 1 = 1
+ where bp.brewery_id = 518
+ order by bp.product_id;
 
--- Listing 1-6. The alternative syntax cross apply
+rem Listing 1-6. The alternative syntax cross apply
+rem cross apply achieves the same result as join lateral
+rem What this means is that for each row in brewery_products, 
+rem the inline view will be applied. 
 
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , top_ys.yr
- , top_ys.yr_qty
-from brewery_products bp
-cross apply(
-   select
-      ys.yr
-    , ys.yr_qty
-   from yearly_sales ys
-   where ys.product_id = bp.product_id
-   order by ys.yr_qty desc
-   fetch first row only
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       top_ys.yr,
+       top_ys.yr_qty
+  from brewery_products bp
+cross apply (
+   select ys.yr,
+          ys.yr_qty
+     from yearly_sales ys
+    where ys.product_id = bp.product_id
+    order by ys.yr_qty desc
+    fetch first row only
 ) top_ys
-where bp.brewery_id = 518
-order by bp.product_id;
+ where bp.brewery_id = 518
+ order by bp.product_id;
 
--- Listing 1-7. Using outer apply when you need outer join functionality
-
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , top_ys.yr
- , top_ys.yr_qty
-from brewery_products bp
-outer apply(
-   select
-      ys.yr
-    , ys.yr_qty
-   from yearly_sales ys
-   where ys.product_id = bp.product_id
-   and ys.yr_qty < 400
-   order by ys.yr_qty desc
-   fetch first row only
+rem Listing 1-7. Using outer apply when you need outer join functionality
+rem this is equivalent to outer join. if any of the product of Balthazar Brauerei
+rem brewary has sales greater than or equal to 400 then it's excluded from the 
+rem inner query result while all the products of from Balthazar Brauerei brewary   
+rem would be selected and outer joined with inner query output.
+rem
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       top_ys.yr,
+       top_ys.yr_qty
+  from practical.brewery_products bp
+outer apply (
+   select ys.yr,
+          ys.yr_qty
+     from practical.yearly_sales ys
+    where ys.product_id = bp.product_id
+      and ys.yr_qty < 500
+    order by ys.yr_qty desc
+    fetch first row only
 ) top_ys
-where bp.brewery_id = 518
-order by bp.product_id;
+ where bp.brewery_id = 518
+ order by bp.product_id;
+rem
+rem the equivalent query with traditional left outer join
+rem
+select brewery_name,
+       p_id,
+       product_name,
+       yr,
+       yr_qty
+  from (
+   select bp.brewery_name,
+          bp.product_id as p_id,
+          bp.product_name,
+          top_ys.yr,
+          top_ys.yr_qty,
+          row_number()
+          over(partition by bp.product_id
+               order by top_ys.yr_qty desc
+          ) as rn
+     from practical.brewery_products bp
+     left outer join (
+      select ys.yr,
+             ys.product_id,
+             ys.yr_qty
+        from practical.yearly_sales ys
+       where ys.yr_qty < 400
+   ) top_ys
+   on top_ys.product_id = bp.product_id
+    where bp.brewery_id = 518
+) bpv
+ where rn = 1
+ order by bpv.p_id;
 
 -- Listing 1-8. Outer join with the lateral keyword
 
-select
-   bp.brewery_name
- , bp.product_id as p_id
- , bp.product_name
- , top_ys.yr
- , top_ys.yr_qty
-from brewery_products bp
-left outer join lateral(
-   select
-      ys.yr
-    , ys.yr_qty
-   from yearly_sales ys
-   where ys.product_id = bp.product_id
-   order by ys.yr_qty desc
-   fetch first row only
+select bp.brewery_name,
+       bp.product_id as p_id,
+       bp.product_name,
+       top_ys.yr,
+       top_ys.yr_qty
+  from practical.brewery_products bp
+  left outer join lateral (
+   select ys.yr,
+          ys.yr_qty
+     from practical.yearly_sales ys
+    where ys.product_id = bp.product_id
+    order by ys.yr_qty desc
+    fetch first row only
 ) top_ys
-   on top_ys.yr_qty < 500
-where bp.brewery_id = 518
-order by bp.product_id;
+on top_ys.yr_qty < 500
+ where bp.brewery_id = 518
+ order by bp.product_id;
 
 /* ***************************************************** */
--- book_practice_scripts/practical-oracle-sql/Chapter 1/work.sql
+++ book_practice_scripts/practical-oracle-sql/Chapter 1/work.sql
@@ -1,3 +1,4 @@
+alter session set current_schema = practical;
 select *
   from practical.yearly_sales ys
  inner join practical.brewery_products bp
@@ -21,3 +22,51 @@ select dbms_metadata.get_ddl(
    'PRACTICAL'
 )
   from dual;       
+--
+select substr('2017;582',1, instr(
+   '2017;582',
+   ';') -1
+) as yr,
+    substr('2017;582', instr(
+       '2017;582',
+       ';') + 1
+) as yr_qty
+  from dual;
+--
+select instr(
+   'Hello World', -- string to search 
+   'o', -- string/character to find
+   1, -- start position
+   2 -- occurrence
+);
+--
+select instr(
+   'Hello World', -- string to search 
+   'o', -- string/character to find
+   1, -- start position
+   1 -- occurrence
+);
+--
+select instr(
+   'Hello World', -- string to search 
+   'o', -- string/character to find
+   -1, -- backward start position
+   1 -- occurrence from the start position
+);
+--
+select ys.yr,
+       ys.yr_qty
+  from yearly_sales ys
+   --  where ys.product_id = bp.product_id
+ order by ys.yr_qty desc
+ fetch first row only;
+ --
+ select ys.yr,
+       ys.yr_qty
+  from yearly_sales ys
+ where ys.product_id in (5310, 5430, 6520)
+  --  and ys.yr_qty < 400
+ order by ys.yr_qty desc;
+ fetch first row only;
+ --
+ select * from practical.brewery_products where brewery_id = 518;
